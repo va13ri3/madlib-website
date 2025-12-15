1635a447dcf95c7521489996f10465ed87413106
Valerie <127vgonzalezmorales@grmail.com>
2025-12-15 14:02:52 -0500

    mdwdfgh

--- js/script.js
+++ js/script.js
@@ -1,44 +1,13 @@
-var adjectiveOne = document.getElementById("adjective");
-var twoWord = document.getElementById("two");
-var anImal = document.getElementById("animal");
-var adjectiveTwo = document.getElementById("adjective2");
-var objectOne = document.getElementById("object");
-var veRb = document.getElementById("verb");
-var objectTwo = document.getElementById("object2");
-var objectThree = document.getElementById("object3");
-var adjectiveThree = document.getElementById("adjective3");
-var objectFour = document.getElementById("object4");
-var butTon = document.getElementById("submitBtn");
-var outPut = document.getElementById("output");
+var button = document.getElementById("submit")
+console.log(button);
+var words = document.querySelectorAll('input[type="text"]');
+console.log(words);
 
 
-console.log(adjectiveOne);
-console.log(twoWord);
-console.log(animal);
-console.log(adjectiveTwo);
-console.log(objectOne);
-console.log(veRb);
-console.log(objectTwo);
-console.log(objectThree);
-console.log(objectFour);
-console.log(butTon);
-console.log(outPut);
+button.addEventListener('click', getWords)
 
-butTon.addEventListener('click', myFunction);
+function getWords() {
+    console.log(button);
+    console.log(words);
+}
 
-function myFunction() {
-    const firstAdj = adjective.value;
-    const doubleAdj = two.value;
-    const animalInput = animal.value;
-    const secondAdj = adjective2.value;
-    const firstObj = object.value;
-    const verbInput = verb.value;
-    const secondObj = object2.value;
-    const thirdObj = object3.value;
-    const thirdAdj = adjective3.value;
-    const fourthObj = object4.value;
-    
-    outPut.innerHTML = ('It was a') + (' ') + (firstAdj)
-     + ('afternoon when the sky above Pickleberry Park suddenly split open with a') + (' ') + (ageUser) + ('<br>') + ('Favorite Color:') + (' ') + (colorUser) + ('<br>') + ('Favorite Pet:') + (' ') + (selectedValue) + ('<br>') + ('Hobbies:') + (' ') + (checkedValues); 
-    }
-    
